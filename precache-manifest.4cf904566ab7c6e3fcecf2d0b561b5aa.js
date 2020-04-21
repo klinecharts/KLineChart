16225794ea59341c0a337bc10d23918a31b7e229
lihu <hu_li888@foxmail.com>
2020-04-22 01:16:03 +0800

    创建主页

---
+++ precache-manifest.4cf904566ab7c6e3fcecf2d0b561b5aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3892af69d8847e21f1b6f1de898ea053",
+    "url": "/KLineChart/index.html"
+  },
+  {
+    "revision": "232129f6a3bb6919b405",
+    "url": "/KLineChart/static/css/main.de02d5ae.chunk.css"
+  },
+  {
+    "revision": "ca1d13e58f9dced9e9d6",
+    "url": "/KLineChart/static/js/2.03e39a9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KLineChart/static/js/2.03e39a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "232129f6a3bb6919b405",
+    "url": "/KLineChart/static/js/main.ef445ea1.chunk.js"
+  },
+  {
+    "revision": "ececc8018ac12fe49949",
+    "url": "/KLineChart/static/js/runtime-main.ef32f9ab.js"
+  }
+]);
--- src/utils/env.js
+++
@@ -1,1 +0,0 @@
-export const DEV = process.env.NODE_ENV === 'development'
