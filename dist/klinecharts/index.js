45692530aebb6dae4c8012233ed7fbda14883c16
SSH <ssh@SSH.local>
2020-05-13 11:22:38 +0800

    rm dist

--- dist/klinecharts/index.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/klinecharts.production.min.js')
-} else {
-  module.exports = require('./dist/klinecharts.development.js')
-}
