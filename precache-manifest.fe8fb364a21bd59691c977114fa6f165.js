416f3aa9e08a6bec2294cb00f7d1270dd09cabdb
lihu <hu_li888@foxmail.com>
2020-07-28 00:31:06 +0800

    Updates

--- precache-manifest.fe8fb364a21bd59691c977114fa6f165.js
+++ precache-manifest.fe8fb364a21bd59691c977114fa6f165.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c2d49e4c044ebd327bfb7d8f27a1f236",
+    "revision": "f2dc4a67997ca28e1f43520f7cd95c26",
     "url": "/KLineChart/index.html"
   },
   {
-    "revision": "cab724265e1eeea27389",
-    "url": "/KLineChart/static/css/main.b8ecdf8c.chunk.css"
+    "revision": "a15f8364e7e2b4988186",
+    "url": "/KLineChart/static/css/main.d7819557.chunk.css"
   },
   {
     "revision": "e0bd2fe4a406f765ca84",
@@ -16,12 +16,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/KLineChart/static/js/2.29bdd034.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cab724265e1eeea27389",
-    "url": "/KLineChart/static/js/main.ae01997d.chunk.js"
+    "revision": "a15f8364e7e2b4988186",
+    "url": "/KLineChart/static/js/main.0dd1e6df.chunk.js"
   },
   {
     "revision": "a8db9746838aec4af8e23e9aaa867338",
-    "url": "/KLineChart/static/js/main.ae01997d.chunk.js.LICENSE.txt"
+    "url": "/KLineChart/static/js/main.0dd1e6df.chunk.js.LICENSE.txt"
   },
   {
     "revision": "dd3418977e5e32552eeb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/KLineChart/precache-manifest.5dab593f5b44985be3ed465b45f57ea8.js"
+  "/KLineChart/precache-manifest.fe8fb364a21bd59691c977114fa6f165.js"
 );
 
 self.addEventListener('message', (event) => {
