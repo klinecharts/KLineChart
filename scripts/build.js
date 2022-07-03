5f69040a2d03dc90bb7e7d92f0483572a6482b8e
lihu <hu_li888@foxmail.com>
2022-07-04 02:40:16 +0800

    ci: build

---
+++ scripts/build.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const rollup = require('rollup');
+
+const chalk = require('chalk');
+
+const { inputConfig, outputConfig } = require('./config');
+
+const type = process.argv[2] !== 'simple' && process.argv[2] !== 'blank' ? 'full' : process.argv[2];
+const env = process.argv[3] !== 'development' ? 'production' : 'development';
+
+async function build() {
+  console.log(`Creating an optimized ${chalk.blue(`${type} ${env}`)} build...\n`);
+  const input = inputConfig(type, env);
+
+  try {
+    const bundle = await rollup.rollup(input);
+
+    console.log('\n\nFile info: ');
+
+    const output = outputConfig(type, env);
+
+    await bundle.write(output);
+
+    console.log(chalk.green(`\nCompiled ${type} ${env} successfully.\n`));
+  } catch (err) {
+    console.log(`\n\n${chalk.red(err)}\n`);
+    console.log(chalk.red(`Failed to compile ${type} ${env}.\n`));
+  }
+}
+
+build();
---
+++ scripts/config.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const { babel } = require('@rollup/plugin-babel');
+const fileSize = require('rollup-plugin-filesize');
+const { terser } = require('rollup-plugin-terser');
+const replace = require('@rollup/plugin-replace');
+const progress = require('rollup-plugin-progress');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const paths = require('./paths');
+
+const packageJson = require(paths.packageJson);
+
+const version = packageJson.version;
+
+const plugins = (env) => [
+  babel({
+    babelHelpers: 'runtime',
+    exclude: '**/node_modules/**'
+  }),
+  nodeResolve(),
+  commonjs(),
+  progress(),
+  fileSize(),
+  replace({
+    '__BUILD_ENV__': env,
+    '__BUILD_VERSION__': version,
+  }),
+  env === 'production' && terser({
+    compress: {
+      pure_getters: true,
+      unsafe: true,
+      unsafe_comps: true
+    }
+  })
+];
+
+function inputConfig (type, env) {
+  const inputPath = () => {
+    switch (type) {
+      case 'simple': return paths.simpleIndexJs;
+      case 'blank': return paths.blankIndexJs;
+      default: return paths.fullIndexJs;
+    }
+  }
+  return { input: inputPath(), plugins: plugins(env) };
+}
+
+function outputConfig (type, env) {
+  const isDevelopment = env === 'development';
+  const fileName = () => {
+    switch (type) {
+      case 'simple': return isDevelopment ? 'klinecharts.simple' : 'klinecharts.simple.min';
+      case 'blank': return isDevelopment ? 'klinecharts.blank' : 'klinecharts.blank.min';
+      default: return isDevelopment ? 'klinecharts' : 'klinecharts.min';
+    }
+  }
+  return {
+    file: `${paths.build}/${fileName()}.js`,
+    format: 'umd',
+    name: 'klinecharts',
+    sourcemap: isDevelopment,
+    indent: false,
+    banner: `
+/**
+ * @license
+ * KLineChart v${version}
+ * Copyright (c) 2019 lihu.
+ * Licensed under Apache License 2.0 https://www.apache.org/licenses/LICENSE-2.0
+ */`.trim(),
+  };
+}
+
+module.exports = { 
+  inputConfig,
+  outputConfig
+};
