60561369a6a8d2a8ffb21e5fbeabca67f9d7147c
liihuu <hu_li888@foxmail.com>
2023-09-22 00:03:27 +0800

    fix: style `crosshair.vertical.text.paddingLeft` error.

--- src/view/CrosshairVerticalLabelView.ts
+++ src/view/CrosshairVerticalLabelView.ts
@@ -14,7 +14,7 @@
 
 import Bounding from '../common/Bounding'
 import Crosshair from '../common/Crosshair'
-import { CrosshairStyle, CrosshairDirectionStyle, StateRectTextStyle, FormatDateType } from '../common/Options'
+import { CrosshairStyle, CrosshairDirectionStyle, StateTextStyle, FormatDateType } from '../common/Options'
 
 import XAxis from '../component/XAxis'
 
@@ -37,16 +37,19 @@ export default class CrosshairVerticalLabelView extends CrosshairHorizontalLabel
     return chartStore.getCustomApi().formatDate(chartStore.getTimeScaleStore().getDateTimeFormat(), timestamp, 'YYYY-MM-DD HH:mm', FormatDateType.Crosshair)
   }
 
-  override getTextAttrs (text: string, textWidth: number, crosshair: Crosshair, bounding: Bounding, _axis: XAxis, styles: StateRectTextStyle): TextAttrs {
+  override getTextAttrs (text: string, textWidth: number, crosshair: Crosshair, bounding: Bounding, _axis: XAxis, styles: StateTextStyle): TextAttrs {
     const x = crosshair.realX as number
     let optimalX: number
+    let align: CanvasTextAlign = 'center'
     if (x - textWidth / 2 - styles.paddingLeft < 0) {
-      optimalX = styles.paddingLeft + textWidth / 2
+      optimalX = 0
+      align = 'left'
     } else if (x + textWidth / 2 + styles.paddingRight > bounding.width) {
-      optimalX = bounding.width - styles.paddingRight - textWidth / 2
+      optimalX = bounding.width
+      align = 'right'
     } else {
       optimalX = x
     }
-    return { x: optimalX, y: 0, text, align: 'center', baseline: 'top' }
+    return { x: optimalX, y: 0, text, align, baseline: 'top' }
   }
 }
