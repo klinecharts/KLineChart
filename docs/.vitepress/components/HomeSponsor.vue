dc65b84038742f69161d5bb9765f4089aa225dce
liihuu <hu_li888@foxmail.com>
2023-09-29 02:48:34 +0800

    docs: update docs

---
+++ docs/.vitepress/components/HomeSponsor.vue
@@ -0,0 +1,81 @@
+<script setup>
+  import { useData } from 'vitepress'
+
+  const { lang } = useData()
+</script>
+
+<template>
+  <section class="home-section sponsor-section">
+    <div class="home-section-content sponsor">
+      <h2>{{ lang === 'zh-CN' ? '赞助商' : 'Sponsors' }}</h2>
+      <div class="grid">
+        <a class="item" href="./sponsor.html" target="_blank" rel="noreferrer">
+          {{ lang === 'zh-CN' ? '成为赞助商' : 'Become a sponsor' }}
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  .sponsor-section {
+    margin-top: 100px;
+  }
+
+  .sponsor h2 {
+    font-size: 24px;
+    text-align: center;
+    font-weight: 600;
+  }
+
+  .sponsor .grid {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 6px;
+    margin-top: 40px;
+  }
+
+  .sponsor .grid .item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 138px;
+    color: var(--vp-c-text-2)!important;
+    background-color: var(--vp-c-bg-soft);
+    border-radius: 4px;
+    transition: background-color .2s;
+    cursor: pointer;
+    color: inherit;
+  }
+
+  .sponsor .grid .item .image {
+    height: 34px;
+  }
+
+  .dark .sponsor .grid .item .image {
+    filter: grayscale(1) invert(1);
+  }
+
+  .dark .sponsor .grid .item:hover {
+    background-color: var(--vp-c-neutral);
+    color: rgba(0, 0, 0, 0.6)!important;
+  }
+
+  .dark .sponsor .grid .item:hover .image {
+    filter: grayscale(0) invert(0);
+  }
+
+  @media (min-width: 640px) {
+    .sponsor .grid .item {
+      width: calc((100% - 12px) / 3);
+    }
+  }
+
+  @media (min-width: 960px) {
+    .sponsor .grid .item {
+      width: calc((100% - 18px) / 4);
+    }
+  }
+</style>
--- docs/en-US/sponsor.md
+++ docs/en-US/sponsor.md
@@ -3,20 +3,24 @@ sidebar: false
 ---
 
 # ❤️ Sponsor
+KLineChart under the Apache License V2 and completely free to use.
 
 If you think the plugin is good, I hope you can click 🌟 on [GitHub](https://github.com/liihuu/KLineChart). If you can reward a cup of ☕️, that would be very much appreciated.
 
-## Alipay
+For organizations or individuals sponsoring KLineChart, if necessary, display the organization's official website or personal homepage on the https://klinecharts.com on the homepage, please provide payment account or address, logo, homepage link, and other information by sending an email to hu_li888@foxmail.com . Those with sponsor amount of $800 will be displayed in the large logo display area.
+
+## Sponsor Channels
+### Alipay
 <img style="width:180px;margin-top:10px" src="/images/alipay_qr_code.png"/>
 
-## Wechat Pay
+### Wechat Pay
 <img style="width:180px;margin-top:10px" src="/images/wechat_pay_qr_code.png"/>
 
-## PayPal
+<!-- ## PayPal
 
-- [paypal](https://paypal.me/liihuu)
+- [paypal](https://paypal.me/liihuu) -->
 
-## Cryptocurrency
+### Cryptocurrency
 
 - BTC: [bc1qnwzukszzk5xfk0zs3sr8etzgctgnrtqts43jzp3khe6gm7xazprsp4y6n3]()
 - ETH: [0xd2c3911654db861e0a2e17415e11a209c1fc3594]()
