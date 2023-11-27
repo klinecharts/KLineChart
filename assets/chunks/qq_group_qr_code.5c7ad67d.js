19f557d884dbac99b7b8065a2d865225ac3bba37
liihuu <hu_li888@foxmail.com>
2023-11-28 02:55:34 +0800

    Updates

--- assets/chunks/qq_group_qr_code.5c7ad67d.js
+++
@@ -1,1 +0,0 @@
-const s="/images/wechat.jpeg",_="/images/qq_group_qr_code.png";export{s as _,_ as a};
--- assets/chunks/wechat_pay_qr_code.ab7c3922.js
+++
@@ -1,1 +0,0 @@
-const _="/images/alipay_qr_code.png",a="/images/wechat_pay_qr_code.png";export{_,a};
