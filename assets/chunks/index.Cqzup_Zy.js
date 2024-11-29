51e4cdd4a2ec83dcac88827b0d08bf59ab6804ba
liihuu <hu_li888@foxmail.com>
2024-11-30 02:53:37 +0800

    Updates

---
+++ assets/chunks/index.Cqzup_Zy.js
@@ -0,0 +1,1 @@
+import{C as r}from"./theme.B3RkYLmB.js";import{o as e,g as o,k as n}from"./framework.D0LcRxHx.js";const s=JSON.parse(`{"js":"import { version } from 'klinecharts';\\n\\nconst v = version();"}`),_={__name:"index",setup(t){return(a,c)=>(e(),o(r,{hiddenChart:!0,code:n(s).js},null,8,["code"]))}};export{_};
