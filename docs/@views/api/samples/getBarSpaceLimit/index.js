089b285e4e13a45eafcc9d15785c2165de2302fa
019327 <wangguoshuai@htsc.com>
2025-07-21 16:41:31 +0800

    feat: 增加setBarSpaceLimit和getBarSpaceLimit文档说明

---
+++ docs/@views/api/samples/getBarSpaceLimit/index.js
@@ -0,0 +1,19 @@
+import { init } from 'klinecharts'
+
+const chart = init('getBarSpaceLimit-chart')
+
+chart.setSymbol({ ticker: 'TestSymbol' })
+chart.setPeriod({ span: 1, type: 'day' })
+chart.setDataLoader({
+  getBars: ({
+    callback
+  }) => {
+    fetch('https://klinecharts.com/datas/kline.json')
+      .then(res => res.json())
+      .then(dataList => {
+        callback(dataList)
+      })
+  }
+})
+
+const barSpaceLimit = chart.getBarSpaceLimit()
