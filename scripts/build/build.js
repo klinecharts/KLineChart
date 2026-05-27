186d88324e6eed93cebfc33377443312cbf922ff
liihuu <hu_li888@foxmail.com>
2026-05-28 03:02:52 +0800

    ci: rollup

--- scripts/build/build.js
+++ scripts/build/build.js
@@ -1,15 +1,7 @@
-import { execFileSync } from 'node:child_process'
 import { styleText } from 'node:util'
-import { build as viteBuild } from 'vite'
+import { rollup } from 'rollup'
 
-import { createBuildConfig, version, env } from './config.js'
-
-function checkInput (index) {
-  if (index.endsWith('.ts')) {
-    execFileSync('pnpm', ['code-lint'], { stdio: 'inherit' })
-    execFileSync('pnpm', ['type-check'], { stdio: 'inherit' })
-  }
-}
+import { createInputConfig, createOutputConfig, version, env } from './config.js'
 
 async function build ({ index, replaceValues, fileName, format, parentDir, name }) {
   const text = `version ${version}${env ? ` ${env} ` : ' '}${format} file`
@@ -18,15 +10,16 @@ async function build ({ index, replaceValues, fileName, format, parentDir, name 
 
   try {
     const startTime = new Date().getTime()
-    checkInput(index)
-    await viteBuild(createBuildConfig({
-      input: index, replaceValues, fileName, format, name, parentDir
+    const bundle = await rollup(createInputConfig({ input: index, replaceValues }))
+
+    await bundle.write(createOutputConfig({
+      fileName, format, name, parentDir
     }))
 
     console.log(styleText('green', `\n✔ Compiled ${text} successfully.\n`))
     console.log(`${styleText('green', '✔')} Done in ${((new Date().getTime() - startTime) / 1000 / 60).toFixed(2)}s.\n`)
   } catch (err) {
-    console.log(`\n\n${styleText('red', String(err))}\n`)
+    console.log(`\n\n${styleText('red', err)}\n`)
     console.log(styleText('red', `✖️ Failed to compile ${text}.\n`))
     process.exit(1)
   }
--- scripts/build/config.js
+++ scripts/build/config.js
@@ -1,4 +1,11 @@
+// import child_process from 'child_process'
+
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
+import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
+import fileSize from 'rollup-plugin-filesize'
+import eslint from '@rollup/plugin-eslint'
 
 import { resolvePath, getVersion } from '../utils.js'
 
@@ -18,64 +25,61 @@ const isProd = env === 'production'
 
 const buildDir = resolvePath('dist')
 
-const banner =
-`
-/**
- * @license
- * KLineChart v${version}
- * Copyright (c) 2019 lihu.
- * Licensed under Apache License 2.0 https://www.apache.org/licenses/LICENSE-2.0
- */`.trim()
-
-function createBuildConfig ({
-  input,
-  replaceValues,
-  fileName, format, name, parentDir
-}) {
-  let outDir
-  if (parentDir) {
-    outDir = resolvePath(parentDir, buildDir)
-  } else {
-    outDir = buildDir
-  }
-
+function createInputConfig ({ input, replaceValues }) {
   return {
-    configFile: false,
-    envFile: false,
-    keepProcessEnv: true,
+    input,
     plugins: [
+      typescript(),
+      eslint({
+        throwOnError: true
+      }),
+      nodeResolve(),
       replace({
-        __VERSION__: version,
-        ...replaceValues
-      })
-    ],
-    build: {
-      target: 'es2015',
-      minify: isProd ? 'oxc' : false,
-      emptyOutDir: false,
-      outDir,
-      sourcemap: isDev,
-      lib: {
-        entry: input,
-        formats: [format],
-        fileName: () => fileName,
-        name
-      },
-      rolldownOptions: {
-        external: format === 'cjs'
-          ? ['./umd/klinecharts.min.js', './umd/klinecharts.js']
-          : [],
-        output: {
-          comments: false,
-          postBanner: banner
+        preventAssignment: true,
+        values: {
+          // '__VERSION__': `v${version}(${commitId.length > 0 ? `${commitId}, ` : ''}${new Date().toISOString()})`,
+          __VERSION__: version,
+          ...replaceValues
         }
-      }
-    }
+      }),
+      fileSize(),
+      isProd && terser()
+    ].filter(p => !!p)
   }
 }
 
+function createOutputConfig ({
+  fileName, format, name, parentDir
+}) {
+  let file
+  if (parentDir) {
+    file = resolvePath(fileName, resolvePath(parentDir, buildDir))
+  } else {
+    file = resolvePath(fileName, buildDir)
+  }
+  const config = {
+    file,
+    format,
+    sourcemap: isDev,
+    indent: false,
+    banner: `
+    /**
+     * @license
+     * KLineChart v${version}
+     * Copyright (c) 2019 lihu.
+     * Licensed under Apache License 2.0 https://www.apache.org/licenses/LICENSE-2.0
+     */`.trim()
+  }
+
+  if (name) {
+    config.name = name
+  }
+  return config
+}
+
 export {
-  createBuildConfig,
+  createInputConfig,
+  createOutputConfig,
   version,
   env,
   isDev,
