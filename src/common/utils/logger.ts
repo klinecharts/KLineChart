c9b1dd7bd4eac93f45d48a593d9418469c67a8bc
liihuu <hu_li888@foxmail.com>
2026-07-21 22:47:39 +0800

    style: code style format

--- src/common/utils/logger.ts
+++ src/common/utils/logger.ts
@@ -15,7 +15,7 @@
 // @ts-nocheck
 const DEV = process.env.NODE_ENV === 'development'
 
-function log (templateText: string, tagStyle: string, messageStyle: string, api: string, invalidParam: string, append: string): void {
+function log(templateText: string, tagStyle: string, messageStyle: string, api: string, invalidParam: string, append: string): void {
   if (DEV) {
     const apiStr = api !== '' ? `Call api \`${api}\`${invalidParam !== '' || append !== '' ? ', ' : '.'}` : ''
     const invalidParamStr = invalidParam !== '' ? `invalid parameter \`${invalidParam}\`${append !== '' ? ', ' : '.'}` : ''
@@ -24,31 +24,14 @@ function log (templateText: string, tagStyle: string, messageStyle: string, api:
   }
 }
 
-export function logWarn (api: string, invalidParam: string, append?: string): void {
-  log(
-    '%c😑 klinecharts warning%c %s%s%s',
-    'padding:3px 4px;border-radius:2px;color:#ffffff;background-color:#FF9600',
-    'color:#FF9600',
-    api, invalidParam, append ?? ''
-  )
+export function logWarn(api: string, invalidParam: string, append?: string): void {
+  log('%c😑 klinecharts warning%c %s%s%s', 'padding:3px 4px;border-radius:2px;color:#ffffff;background-color:#FF9600', 'color:#FF9600', api, invalidParam, append ?? '')
 }
 
-export function logError (api: string, invalidParam: string, append?: string): void {
-  log(
-    '%c😟 klinecharts error%c %s%s%s',
-    'padding:3px 4px;border-radius:2px;color:#ffffff;background-color:#F92855;',
-    'color:#F92855;',
-    api, invalidParam, append ?? ''
-  )
+export function logError(api: string, invalidParam: string, append?: string): void {
+  log('%c😟 klinecharts error%c %s%s%s', 'padding:3px 4px;border-radius:2px;color:#ffffff;background-color:#F92855;', 'color:#F92855;', api, invalidParam, append ?? '')
 }
 
-export function logTag (): void {
-  log(
-    '%c❤️ Welcome to klinecharts. Version is __VERSION__',
-    'border-radius:4px;border:dashed 1px #1677FF;line-height:70px;padding:0 20px;margin:16px 0;font-size:14px;color:#1677FF;',
-    '',
-    '',
-    '',
-    ''
-  )
+export function logTag(): void {
+  log('%c❤️ Welcome to klinecharts. Version is __VERSION__', 'border-radius:4px;border:dashed 1px #1677FF;line-height:70px;padding:0 20px;margin:16px 0;font-size:14px;color:#1677FF;', '', '', '', '')
 }
